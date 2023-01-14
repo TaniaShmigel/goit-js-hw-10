a127d0c9aa8ee57e035e0b477b2c625fc29e11a6
TaniaShmigel <tatjanka222@gmail.com>
2023-01-14 20:44:21 +0200

    add task

---
+++ src/fetchCountries.js
@@ -0,0 +1,26 @@
+export default class FetchCountrise {
+  constructor() {
+    this.nameCountry = '';
+  }
+
+  fetch() {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${this.nameCountry}?fields=name,capital,population,flags,languages`
+    )
+      .then(response => response.json())
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+
+  get name() {
+    return this.nameCountry;
+  }
+
+  set name(name) {
+    this.nameCountry = name;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,103 @@
 import './css/styles.css';
+import FetchCountrise from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    input: document.querySelector('#search-box')
+  searchBox: document.querySelector('#search-box'),
+  coutryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+const fetchCountries = new FetchCountrise();
+
+refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  const country = e.target.value.trim();
+
+  if (country === '') {
+    clearCountryList();
+    return (refs.countryInfo.innerHTML = '');
+  }
+
+  fetchCountries.name = country;
+  fetchCountries.fetch().then(data => {
+    arrayCheckLength(data);
+    return data;
+  });
+
+  // console.log(fetchCountries)
+}
+
+function arrayCheckLength(array) {
+  if (array.length >= 1 && array.length > 10) {
+    clearCountryList();
+    clearCountryInfo();
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+
+  if (array.status === 404) {
+    clearCountryList();
+    clearCountryInfo();
+    return Notify.failure('Oops, there is no country with that name');
+  }
+  if (array.length === 1) {
+    clearCountryList();
+    clearCountryInfo();
+    return createCard(array);
+  }
+  clearCountryList();
+  clearCountryInfo();
+  return createList(array);
+}
+
+function createCard(array) {
+  const markupCard = array
+    .map(
+      ({
+        flags: { svg: flags },
+        name: { official, common },
+        capital,
+        population,
+        languages,
+      }) =>
+        `<div class="container"><h2 class="country">
+     <img class="flag" src="${flags}" alt="flag"> 
+        ${official}</h2>
+    <p class="title"><span class="span">Capital: </span>${capital}</p>
+    <p class="title"><span class="span">Population: </span>${population}</p>
+ <p class="title"><span class="span">Languages: </span>${Object.values(
+   languages
+ )}</p></div>`
+    )
+    .join('');
+  refs.countryInfo.insertAdjacentHTML('beforeend', markupCard);
+}
+
+function createList(array) {
+  const markupList = array
+    .map(
+      ({
+        flags: { svg: flags },
+        name: { official, common },
+      }) => `<li class="item">
+         <img class="icon" src="${flags}" alt="flag">
+         <p class="text">${common}</p>
+     </li>  `
+    )
+    .join('');
+  refs.countryInfo.insertAdjacentHTML('beforeend', markupList);
+}
+
+function clearCountryList() {
+  return (refs.coutryList.innerHTML = '');
+}
+
+function clearCountryInfo() {
+  return (refs.countryInfo.innerHTML = '');
 }
